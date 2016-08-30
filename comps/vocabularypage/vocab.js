7f51f1206d4cfcfaa91efa01181f144c7d2f9369
deb bodahl <bodahl.deb@gmail.com>
2016-08-30 16:18:30 -0600

    newstuff

--- comps/vocabularypage/vocab.js
+++ comps/vocabularypage/vocab.js
@@ -3,7 +3,7 @@
   angular.module('kingdoms')
     .component('vocabularypageComponent', {
       templateUrl: 'comps/vocabularypage/vocab.html',
-      controller: moreinfopageController
+      controller: vocabularypageController
     })
 
     function vocabularypageController(listService){
